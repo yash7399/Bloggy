f3e1d4f72ee82ffc2385e510930b27cdc1791188
Yash Shukla <yashh3004@gmail.com>
2025-08-13 13:25:33 +0530

    Some basic changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
-export const connectDB = async() => {
-    try {
-        mongoose.connection.on("connected", () => {
-            console.log("MONGODB CONNECTED SUCCESSFULLY");
-        });
-
-        await mongoose.connect(`${process.env.MONGODB_URI}/blog-app`);
-    } catch (error) {
-        console.log("Error Connecting to MONGODB", error);
-        process.exit(1);
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/blog-app`);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
